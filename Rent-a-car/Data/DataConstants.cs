523cacebfe70595c773374a65b64287468eef4f2
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-03-13 17:56:55 +0200

    commit

--- Rent-a-car/Data/DataConstants.cs
+++ Rent-a-car/Data/DataConstants.cs
@@ -2,5 +2,16 @@
 {
     public class DataConstants
     {
+        public class ApplicationUser
+        {
+            public const int UserFirstNameMaxLength = 12;
+            public const int UserFirstNameMinLength = 1;
+
+            public const int UserLastNameMaxLength = 15;
+            public const int UserLastNameMinLength = 3;
+
+            public const int UserMiddleNameMaxLength = 15;
+            public const int UserMiddleNameMinLength = 3;
+        }
     }
 }
